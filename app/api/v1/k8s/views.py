798a5df6615f0b96970df4d3ef91d9c2572c4f40
praszxc <bpras747@gmail.com>
2023-12-05 15:24:34 +0000

    Integrate k8s #2

--- app/api/v1/k8s/views.py
+++ app/api/v1/k8s/views.py
@@ -26,42 +26,88 @@ def endpoints(request):
 
 ###k8s - GET PODS###
 def GetPods(request):
-    if request.method=='GET':
-        v1 = client.CoreV1Api() 
+    if request.method == 'GET':
+        v1 = client.CoreV1Api()
         pods_list = v1.list_pod_for_all_namespaces()
-    return HttpResponse(pods_list)
+
+        # Convert the pods_list to a dictionary
+        pods_dict = {}
+        for pod in pods_list.items:
+            pods_dict[pod.metadata.name] = {
+                'namespace': pod.metadata.namespace,
+                'status': pod.status.phase,
+                # Add more fields as needed
+            }
+
+        # Return JsonResponse with the pods_dict
+        return JsonResponse(pods_dict)
+
+    # Handle other HTTP methods if needed
+    return HttpResponse("Method not allowed", status=405)
 
 
 ###k8s - GET NAMESPACES###
 def GetNamespaces(request):
-    if request.method=='GET':
-        v1 = client.CoreV1Api()       
+    if request.method == 'GET':
+        v1 = client.CoreV1Api()
         ns_list = v1.list_namespace()
-    return HttpResponse(ns_list)
+
+        # Convert the ns_list to a list of namespace names
+        namespaces = [ns.metadata.name for ns in ns_list.items]
+
+        # Return JsonResponse with the list of namespaces
+        return JsonResponse({'namespaces': namespaces})
+
+    # Handle other HTTP methods if needed
+    return HttpResponse("Method not allowed", status=405)
 
 
 ###k8s - GET NODES###
 def GetNodes(request):
-    if request.method=='GET':
+    if request.method == 'GET':
         v1 = client.CoreV1Api()
         nodes_list = v1.list_node()
-    return HttpResponse(nodes_list)
+
+        # Convert the nodes_list to a list of node names
+        nodes = [node.metadata.name for node in nodes_list.items]
+
+        # Return JsonResponse with the list of nodes
+        return JsonResponse({'nodes': nodes})
+
+    # Handle other HTTP methods if needed
+    return HttpResponse("Method not allowed", status=405)
 
 
 ###k8s - GET DEPLOYMENTS###
 def GetDeployments(request):
-    if request.method=='GET':
-        v1 = client.AppsV1Api() 
-        deployments_list: client.V1DeploymentList = v1.list_deployment_for_all_namespaces()
-    return HttpResponse(deployments_list)
+    if request.method == 'GET':
+        v1 = client.AppsV1Api()
+        deployments_list = v1.list_deployment_for_all_namespaces()
+
+        # Convert the deployments_list to a list of deployment names
+        deployments = [deployment.metadata.name for deployment in deployments_list.items]
+
+        # Return JsonResponse with the list of deployments
+        return JsonResponse({'deployments': deployments})
+
+    # Handle other HTTP methods if needed
+    return HttpResponse("Method not allowed", status=405)
 
 
 ###k8s - GET SERVICES###
 def GetServices(request):
-    if request.method=='GET':
-        v1 = client.CoreV1Api() 
+    if request.method == 'GET':
+        v1 = client.CoreV1Api()
         services_list = v1.list_service_for_all_namespaces()
-    return HttpResponse(services_list)
+
+        # Convert the services_list to a list of service names
+        services = [service.metadata.name for service in services_list.items]
+
+        # Return JsonResponse with the list of services
+        return JsonResponse({'services': services})
+
+    # Handle other HTTP methods if needed
+    return HttpResponse("Method not allowed", status=405)
 
 
 ###k8s - CREATE POD###
